e2db76accc1df4b13217524c4041430a8d14dd10
Ishan Voleti <ishan.voleti@gmail.com>
2023-03-14 17:34:16 -0400

    turn function, indexerTask, flywheel improvements

--- src/subsystemFiles/flywheel.cpp
+++ src/subsystemFiles/flywheel.cpp
@@ -1,12 +1,35 @@
 #include "main.h"
 
+int mode = 0;
+
 //HELPER FUNCTIONS
-void setFlywheel(int power){
+void setFlywheel(int power)
+{
   flywheel = power;
 }
 
 //DRIVER CONTROL FUNCTIONS
-void setFlywheelMotor() {
-  int flywheelPower = -127 * (controller.get_digital(pros::E_CONTROLLER_DIGITAL_DOWN));
-  setFlywheel(flywheelPower);
+void setFlywheelMotor() 
+{
+  if(controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_DOWN))
+  {
+    mode++;
+  }
+
+  if(mode > 1)
+  {
+    mode = 0;
+  }
+
+  if(mode == 0)
+  {
+    setFlywheel(0);
+  }
+
+  if(mode == 1)
+  {
+    //adjust the speed value
+    setFlywheel(-127);
+  }
+
 }
